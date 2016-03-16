a6635050f54dd4d9358334938a5568a11a2b655c
Danny Santos <danny@ethicalswitch.com>
2016-03-16 14:18:01 +0000

    Initialize OrangePeril

--- index.php
+++ index.php
@@ -1,10 +1,35 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
+    
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    
     <title>Ethical Living Challenge</title>
 </head>
 <body>
-    <h1></h1>
+    <header class="container">
+        <div class="row">
+            <div class="col-sm-8">
+                <h1>Ethical Living Challenge</h1>
+            </div>
+        
+            <nav class="navbar col-sm-4">
+                <div class="container">
+                    <ul class="nav navbar-nav">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="#" class="btn btn-primary">Sign up!</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </header>
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script src="js/script.js"></script>
 </body>
 </html>
