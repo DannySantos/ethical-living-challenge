335c01cfb1abc8bb3c47215f284a6c2319c8088b
Danny Santos <danny@ethicalswitch.com>
2016-03-18 15:01:27 +0000

    Basic structure

--- index.php
+++ index.php
@@ -4,32 +4,186 @@
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-    
     <title>Ethical Living Challenge</title>
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-    <header class="container">
-        <div class="row">
-            <div class="col-sm-8">
-                <h1>Ethical Living Challenge</h1>
+    <div class="container-fluid full-wrapper text-center">
+        <header>
+            <div class="row">
+                <div class="col-sm-5 col-sm-offset-1 pull-left text-left">
+                    <h1 class="text-hide">
+                        <img src="images/logo.png" alt="Ethical Living Challenge Logo">
+                        Ethical Living Challenge
+                    </h1>
+                </div>
+
+                <nav class="navbar col-sm-6">
+                    <div class="navbar-header pull-right">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapsemenu">
+                            <span class="glyphicon glyphicon-align-justify"></span>
+                        </button>
+                    </div>
+                    <div class="collapse navbar-collapse pull-right" id="collapsemenu">
+                        <ul class="nav navbar-nav">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">FAQ</a></li>
+                            <li><a href="#" class="btn btn-pop">Sign up!</a></li>
+                        </ul>
+                    </div>
+                </nav>
             </div>
         
-            <nav class="navbar col-sm-4">
-                <div class="container">
-                    <ul class="nav navbar-nav">
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">FAQ</a></li>
-                        <li><a href="#" class="btn btn-primary">Sign up!</a></li>
-                    </ul>
-                </div>
-            </nav>
+            
+            <img src="images/header-bg-curve.png" alt="Header Style" class="full-width">
+        </header>
+
+        <div class="row">
+            <h2>Welcome to the<br>Ethical Living Challenge!</h2>
+
+            <p class="col-sm-6 col-sm-offset-3">An annual 21-day online challenge to live more ethically. Learn, engage and act with daily information, challenges, resources and competitions sent directly to your inbox.</p>
+
+            <div class="clearfix"></div>
+
+            <form class="form-inline wider-form">
+                <input type="email" placeholder="Email" class="form-control">
+                <input type="submit" class="form-control btn-pop" value="Sign up!">
+            </form>
         </div>
-    </header>
+        
+        <div class="row bg-divider top-margin">
+            <div class="col-sm-10 col-sm-offset-1">
+                <p class="large-text">"Every time you spend money, you're casting a vote for the kind of world you want."</p>
+                <p>-Anna Lapp&eacute;</p>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-sm-10 col-sm-offset-1">
+                <div class="carousel slide top-margin" data-ride="carousel" id="elcCarousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#elcCarousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#elcCarousel" data-slide-to="1"></li>
+                        <li data-target="#elcCarousel" data-slide-to="2"></li>
+                        <li data-target="#elcCarousel" data-slide-to="3"></li>
+                    </ol>
+
+                    <div class="carousel-inner">
+                        <div class="item active">
+                            <img src="images/carousel-1.jpg" alt="">
+                            <div class="carousel-caption">
+                                <h3>Caption Header</h3>
+                                <p>We can put whatever images you want in here and give them some captions/links</p>
+                            </div>
+                        </div>
+
+                        <div class="item">
+                            <img src="images/carousel-2.jpg" alt="">
+                            <div class="carousel-caption">
+                                <h3>Caption Header</h3>
+                                <p>We can put whatever images you want in here and give them some captions/links</p>
+                            </div>
+                        </div>
+
+                        <div class="item">
+                            <img src="images/carousel-3.jpg" alt="">
+                            <div class="carousel-caption">
+                                <h3>Caption Header</h3>
+                                <p>We can put whatever images you want in here and give them some captions/links</p>
+                            </div>
+                        </div>
+
+                        <div class="item">
+                            <img src="images/carousel-4.jpg" alt="">
+                            <div class="carousel-caption">
+                                <h3>Caption Header</h3>
+                                <p>We can put whatever images you want in here and give them some captions/links</p>
+                            </div>
+                        </div>
+
+                        <a href="#elcCarousel" data-slide="prev" class="left carousel-control">
+                            <span class="glyphicon glyphicon-chevron-left"></span>
+                        </a>
+
+                        <a href="#elcCarousel" data-slide="next" class="right carousel-control">
+                            <span class="glyphicon glyphicon-chevron-right"></span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="row">
+            <h2>Info Section</h2>
+        </div>
+        
+        <div class="row">
+            <div class="col-sm-10 col-sm-offset-1">
+                    <blockquote>
+                    "The Fair Trade Association is proud to support the Ethical Living Challenge. As consumers we have the power to cast votes about the kind of world in which we want to live. The Ethical Living Challenge enables young people to gain unique insights into the way their consumer vote can be made for products and businesses that provide fair wages and conditions throughout the supply chain."
+                    <footer>Chris Hartley, Executive Director, Fair Trade Association</footer>
+                    </blockquote>
+                </div>
+
+            <div class="col-sm-10 col-sm-offset-1">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <blockquote>
+                        "Ethical Switch is proud to sponsor the Ethical Living Challenge. Our mission is to empower consumers to swap their daily spending to better options without unnecessary hassle and cost.  We have partnered with the Ethical Living Challenge because we believe that the world may be changed with simple decisions about what is fair and right for the products that we buy."
+                        <footer>Tamlyn Rudolph, Executive Director, Ethical Switch</footer>
+                        </blockquote>
+                    </div>
+
+                    <div class="col-sm-6">
+                        <blockquote>
+                        "Child, slave and sweatshop labour are all too real in 2015... and nothing is going to change as long as we all continue to shop and consume blindly. The Ethical Living Challenge is a brilliant idea because it encourages people to consume according to their values... much easier said than done!"
+                        <footer>Nick Savaidis, Founder, Etiko</footer>
+                        </blockquote>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="row bg-divider">
+            <h2>Start the challenge today!</h2>
+            
+            <form class="form-inline wider-form">
+                <input type="email" placeholder="Email" class="form-control">
+                <input type="submit" class="form-control btn-pop" value="Sign up!">
+            </form>
+        </div>
+        
+        <div class="row partners">
+            <h2>Our Partners</h2>
+            
+            <div class="col-sm-4">
+                <a href="http://ethicalswitch.com/" target="_blank">
+                    <img src="images/partner-ethical-switch.png" alt="Ethical Switch">
+                </a>
+            </div> 
+            
+            <div class="col-sm-4">
+                <a href="http://www.fta.org.au/" target="_blank">
+                    <img src="images/partner-fta.jpg" alt="Fair Trade Association">
+                </a>
+            </div> 
+            
+            <div class="col-sm-4">
+                <a href="http://www.etiko.com.au/" target="_blank">
+                    <img src="images/partner-etiko.png" alt="Etiko">
+                </a>
+            </div>   
+        </div>
+        
+        <footer class="row main-footer">
+            <img src="images/logo.png" alt="Ethical Living Challenge Logo">
+        </footer>
+    </div>
     
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-    <script src="js/script.js"></script>
+    <script src="js/scripts.js"></script>
 </body>
 </html>
