d9e5eb4563992727beb9660396cccaed3f2279ec
Danny Santos <dannyangelosanto@gmail.com>
2018-07-04 17:05:36 +0100

    Bootstrap

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
+    
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    
     <title>Ethical Living Challenge</title>
 </head>
 <body>
-    <h1></h1>
+    <header class="container-fluid">
+        <h1>Ethical Living Challenge</h1>
+    </header>
 </body>
 </html>
